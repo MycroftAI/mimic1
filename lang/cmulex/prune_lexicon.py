b6b95c0a39199b3d454eb842b636b411c85f0145
Sergio Oller <sergioller@gmail.com>
2016-03-15 03:09:11 +0100

    The python script is able to test the LTS rules. Improved python docstring a bit

--- lang/cmulex/prune_lexicon.py
+++ lang/cmulex/prune_lexicon.py
@@ -71,7 +71,7 @@ def atom(token):
 #############################################################################
 
 
-def read_lexicon(filename):
+def read_raw_lexicon(filename):
     """ Reads a lexicon in Festival format
     The festival format consists of a text file with one entry per line.
     Each entry has three parts:
@@ -94,34 +94,43 @@ def read_lexicon(filename):
             yield parse(line)
 
 
-def lexicon_to_dict(lexicon):
+def read_lexicon(filename):
     """ Converts the lexicon, as parsed by read_lexicon into a
-    word->transcription dict.
+    word->[(part of speech1, phones in syllables1, phones1),
+           (part of speech2, phones in syllables2, phones2), ...] dictionary.
     """
     output = defaultdict(list)
-    for line in lexicon:
+    for line in read_raw_lexicon(filename):
         word = line[0]
         pos = line[1]
         syls = line[2]
-        #transcr = flatten_syls(syls)
-        output[word].append((pos, syls))
+        flattened_syls = flatten_syls(syls)
+        output[word].append((pos, syls, flattened_syls))
     return output
 
+def read_raw_align(filename):
+    """ Reads a lex.align """
+    with open(filename, "rt") as fd:
+        line = fd.readline().rstrip()
+        if line != "MNCL":
+            yield parse(line)
+        for line in fd:
+            yield parse(line)
 
-def flatten_lexicon(lexicon):
-    """ Converts the lexicon, as parsed by read_lexicon into a
-    word->transcription dict.
-    In this conversion process the syllabification is flattened, as we don't
-    want it.
+
+def read_align(filename):
+    """ Converts the align, as parsed by read_align into a
+    word->[(part of speech1, phones in syllables1, phones1),
+           (part of speech2, phones in syllables2, phones2), ...] dictionary.
     """
-    output = defaultdict(list)
-    for line in lexicon:
+    output = []
+    for line in read_raw_align(filename):
         word = line[0]
         pos = line[1]
-        syls = line[2]
-        transcr = flatten_syls(syls)
-        output[word].append((pos, transcr))
+        phones = line[2:]
+        output.append([word, pos, phones])
     return output
+
 
 
 def flatten_syls(syls):
@@ -140,7 +149,7 @@ def flatten_syls(syls):
 
 
 def read_lts(filename):
-    """ This parses an _lts_rules.scm file. It contains the decision tree
+    """ This parses a _lts_rules.scm file. It contains the decision tree
     that is used to map words not present in the lexicon to phonemes."""
     with open(filename, "rt") as fd:
         output = []
@@ -168,7 +177,7 @@ def process_lts(lts):
     This function returns a dictionary. The keys of the dictionary are the
     letters. The values are the decision trees for each letter, expressed
     as a lambda function that takes as input a word and an index and returns
-    the phoneme associated to that word.
+    the phoneme associated to that word[index] in its context.
     """
     output = dict()
     for character_tree in lts:
@@ -181,7 +190,8 @@ def process_lts(lts):
 
 def eval_tree(tree):
     """ This function takes an LTS tree as parsed by the scheme interpreter
-    and returns a lambda function.
+    and returns a lambda function. The lambda function takes a word and an index
+    returning the phoneme associated to that word[index].
     """
     # non-final node: Depending on condition we follow one branch or another
     if len(tree) == 3:
@@ -192,16 +202,14 @@ def eval_tree(tree):
                 condition(word, index) else fun_no(word, index))
     # final node, we take the most option with highest probability
     elif len(tree) == 1:
-        options = dict(((y, x) for (x, y) in tree[0][:-1]))
-        maxkey = max(options.keys())
-        return lambda word, index: options[maxkey]
+        return lambda word, index: tree[0][-1]
     else:
         # should not happen
         raise NotImplementedError("Tree: {}".format(tree))
 
 
 def eval_condition(condition):
-    "Evaluates a tree condition"
+    "Generates a lambda function for a condition in the scheme decision tree"
     # "n.name is 'p'"
     if (len(condition) == 3 and isinstance(condition[0], str) and
             condition[1] == "is"):
@@ -237,59 +245,115 @@ def parse_feat(feat):
     return offset
 
 
-def predict(word, pos=None, lexicon=None, lts=None):
-    """To predict a word we use the lexicon and, as fallback the letter to
-    sound rules.
+def predict_lex(word, pos=None, lexicon=None, flattened=True):
+    """ Predicts the phonetic transcription of word with part of speech `pos`
+        using lexicon. If word is not in lexicon returns None.
+        `flattened` is a boolean that determines if the prediction should
+        be the phones with syllabification or the flattened phones
     """
-    wordpred = None
-    if lexicon is not None:
-        wordpred = lexicon.get(word, [])
-        if len(wordpred == 0):
-            return predict_lts(word, lts)
-        elif len(wordpred == 1): # no disambiguation
-            return wordpred[0][1]
+    if lexicon is None:
+        return None
+    if flattened:
+        homograph_trans = 2
+    else:
+        homograph_trans = 1
+    homographs = lexicon.get(word, [])
+    if len(homographs) == 0:
+        return None
+    elif len(homographs) == 1 or pos is None:
+        return homographs[0][homograph_trans]
+    else:
+        # pos->trans
+        homograph_dict = dict([(x[0], x[homograph_trans]) for x in homographs])
+        if pos in homograph_dict.keys():
+            return homograph_dict[pos]
         else:
-            pos_trans_dict = dict(wordpred)
-            if pos is not None and pos in pos_trans_dict.keys():
-                return pos_trans_dict[pos]
-            else:
-                return predict_lts(word, lts)
+            return homographs[0][homograph_trans]
 
-def predict_lts(word, lts_lambda):
+
+def predict_lts(word, lts_lambda, silences=True):
     """
     To predict using Letter To Sound Rules we pad the word with zeros (as done
     in Festival LTS training. Then we choose the right tree for each character
     in our word and predict the phonemes, that are returned in a list.)
     """
     phonemes = []
-    word_list = [0, 0, 0] + list(word) + [0, 0, 0]
+    word_list = [0, 0, "#"] + list(word) + ["#", 0, 0]
     for word_index in range(3, len(word_list)-3):
         word_char = word_list[word_index]
         tree_lambda = lts_lambda[word_char]
         phone = tree_lambda(word_list, word_index)
-        #if phone != "_epsilon_":
         phonemes.append(phone)
-    return phonemes
+    if silences:
+        return phonemes
+    else:
+        return [x for x in phonemes if x != "_epsilon_"]
+
+def predict_letters(letters, lts_lambda, silences=True):
+    """
+    To predict using Letter To Sound Rules we pad the word with zeros (as done
+    in Festival LTS training. Then we choose the right tree for each character
+    in our word and predict the phonemes, that are returned in a list.)
+    """
+    phonemes = []
+    word_list = [0, 0, '#'] + letters + ["#", 0, 0]
+    for word_index in range(3, len(word_list)-3):
+        word_char = word_list[word_index]
+        tree_lambda = lts_lambda[word_char]
+        phone = tree_lambda(word_list, word_index)
+        phonemes.append(phone)
+    if silences:
+        return phonemes
+    else:
+        return [x for x in phonemes if x != "_epsilon_"]
 
 
-def prune_lexicon(lexicon, lts, lexicon_with_syl):
-    """Predicts all the lexicon words with lts rules, keeping in a dictionary
-    the words that are not predicted correctly"""
+
+def predict(word, pos=None, lexicon=None, lts=None):
+    """To predict a word we use the lexicon and, as fallback the letter to
+    sound rules.
+    """
+    wordpred = predict_lex(word, pos, lexicon)
+    if wordpred is None:
+        return predict_lts(word, lts)
+    else:
+        return wordpred
+
+def phone_normalize(phones):
+    output = []
+    for phone in phones:
+        if phone == "_epsilon_":
+            continue
+        output.extend(phone.split("-"))
+    return output
+
+def test_lts(align, lts):
+    """Accuracy of the lts applied to lexicon"""
     count_word_right = 0
     count_word_wrong = 0
+    for letters, pos, phones  in align:
+        translts = predict_letters(letters, lts, silences=True)
+        norm_phones = phone_normalize(phones)
+        norm_translts = phone_normalize(translts)
+        if all([ x==y for (x, y) in zip(norm_phones, norm_translts)]):
+            count_word_right += 1
+        else:
+            print("".join(letters), list(zip(norm_phones, norm_translts)))
+            count_word_wrong += 1
+    accuracy = count_word_right/(count_word_right + count_word_wrong)
+    return accuracy
+
+def prune_lexicon(lexicon, lts):
+    """Predicts all the lexicon words with lts rules, keeping in a dictionary
+    the words that are not predicted correctly"""
     pruned_lex = defaultdict(list)
-    for word, homographs in lexicon_with_syl.items():
-        lowerca = word.lower()
-        for (pos, trans_syl) in homographs:
-            translts = predict_lts(lowerca, lts)
-            trans = lexicon[word]
-            if trans == translts:
-                count_word_right += 1
-            else:
-                count_word_wrong += 1
-                #pruned_lex[word] = (pos, trans_syl)
-                pruned_lex[word].append((pos, trans))
-    return (pruned_lex, count_word_right, count_word_wrong)
+    for word, homographs in lexicon.items():
+        word_lower = word.lower()
+        for (pos, syl, trans) in homographs:
+            translts = predict_lts(word_lower, lts, silences=False)
+            if trans != translts:
+                pruned_lex[word_lower].append((pos, syl, trans))
+    return pruned_lex
 
 
 def write_syls(syls):
@@ -308,31 +372,37 @@ def write_syls(syls):
     return "".join(output)
 
 
-def write_lex(pruned_lex, filename):
+def write_lex(pruned_lex, filename, flattened=True):
     with open(filename, "w") as fd:
         for word in sorted(pruned_lex.keys()):
-            for (pos, syls) in pruned_lex[word]:
-                if isinstance(syls[0], list):
-                    # dict with syllable structure, we have to flatten it
-                    written_phones = write_syls(syls)
+            for pos_syls_flattened in pruned_lex[word]:
+                (pos, syls, flattened_phones) = pos_syls_flattened
+                if flattened:
+                    if flattened_phones is None:
+                        phones_out = write_syls(syls)
+                    else:
+                        phones_out = "(" + " ".join(flattened_phones) + " )"
                 else:
-                    # already flattened
-                    written_phones = "(" + " ".join(syls) + ")"
-                out = '("' + word + '" ' + pos + " " + written_phones + ")"
+                    # take syls and convert it to a scheme string
+                    raise NotImplementedError("Not needed")
+                out = '( "' + word + '" ' + pos + " " + phones_out + ")"
                 print(out, file=fd)
 
+def load_and_test_lts(align_fn, lts_rules_fn):
+    align = read_align(align_fn)
+    lts_raw = read_lts(lts_rules_fn)
+    lts = process_lts(lts_raw)
+    accuracy = test_lts(align, lts)
+    print("LTS word accuracy on train set (cmulex expected ~60%): {:.1%}".
+          format(accuracy))
 
 
 def load_and_prune_lex(lexicon_fn, lts_rules_fn, output_pruned_lex_fn):
-    lexicon_raw = read_lexicon(lexicon_fn)
-    lexicon = flatten_lexicon(lexicon_raw)
-    lexicon_raw = read_lexicon(lexicon_fn)
-    lexicon_with_syl = lexicon_to_dict(lexicon_raw)
+    lexicon = read_lexicon(lexicon_fn)
     lts_raw = read_lts(lts_rules_fn)
     lts = process_lts(lts_raw)
-    (pruned_lex, count_right, count_wrong) = prune_lexicon(lexicon, lts,
-                                                           lexicon_with_syl)
-    write_lex(pruned_lex, output_pruned_lex_fn)
+    pruned_lex = prune_lexicon(lexicon, lts)
+    write_lex(pruned_lex, output_pruned_lex_fn, flattened=True)
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Helpers to create lexicon and LTS rules.')
@@ -342,17 +412,29 @@ def parse_args():
     parser.add_argument('--remove-short', dest='remove_short', action='store_const',
                         const=True, default=None,
                         help='Remove all lexicon entries with 3 words or less for LTS training.')
+    parser.add_argument('--test-lts', dest='test_lts', action='store_const',
+                        const=True, default=None,
+                        help='Test LTS rules.')
     args = parser.parse_args()
     return args
 
-def load_and_filter_lex_for_lts(lexicon_fn, filtered_lex_fn):
-    lexicon_raw = read_lexicon(lexicon_fn)
-    lexicon = flatten_lexicon(lexicon_raw)
-    filtered_lex = dict()
+def filter_lexicon(lexicon):
+    filtered_lex = defaultdict(list)
     for word in lexicon.keys():
         if len(word) > 3:
             filtered_lex[word] = lexicon[word]
-    write_lex(filtered_lex, filtered_lex_fn)
+    return filtered_lex
+
+def load_and_filter_lex_for_lts(lexicon_fn, filtered_lex_fn):
+    """ We load the raw lexicon and return a lexicon with words with more than 
+        three characters. Ideally we could leave nouns, verbs and adjectives as
+        the unknown words we will find will probably belong to one of those 
+        categories. 
+        See: http://www.festvox.org/docs/manual-2.4.0/festival_13.html#Building-letter-to-sound-rules
+    """
+    lexicon = read_lexicon(lexicon_fn)
+    filtered_lex = filter_lexicon(lexicon)
+    write_lex(filtered_lex, filtered_lex_fn, flattened=True)
 
 
 if __name__ == "__main__":
@@ -366,5 +448,8 @@ if __name__ == "__main__":
         lexicon_fn = "festival/lib/dicts/cmu/cmudict-0.4.out"
         filtered_lex_fn = "festival/lib/dicts/cmu/lts_scratch/lex_entries.out"
         load_and_filter_lex_for_lts(lexicon_fn, filtered_lex_fn)
-
+    elif args.test_lts:
+        align_fn = "festival/lib/dicts/cmu/lts_scratch/lex.align"
+        lts_rules_fn = "festival/lib/dicts/cmu/lex_lts_rules.scm"
+        load_and_test_lts(align_fn, lts_rules_fn)
 
