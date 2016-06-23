6d7bcd2564846bf4958428e5336782d0561da30f
Åke Forslund <ake.forslund@gmail.com>
2016-06-23 18:59:11 +0200

    First attempt to precompile the voice of Alan Pope

---
+++ lang/vid_gb_ap/voxdefs.h
@@ -0,0 +1,6 @@
+#define VOXNAME cmu_us_vocalid
+#define REGISTER_VOX register_cmu_us_vocalid
+#define UNREGISTER_VOX unregister_cmu_us_vocalid
+#define VOXHUMAN "ap"
+#define VOXGENDER "unknown"
+#define VOXVERSION 1.0
