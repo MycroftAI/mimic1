8768cf1596ba4d05d47459578cafe69309ccde58
Sergio Oller <sergioller@gmail.com>
2016-07-15 18:03:31 +0200

    Do not test multithreading if openmp is not available

--- testsuite/multi_thread_main.c
+++ testsuite/multi_thread_main.c
@@ -40,7 +40,10 @@
 /*                                                                       */
 /*************************************************************************/
 #include <stdio.h>
-#include <omp.h>
+
+#ifdef _OPENMP
+  #include <omp.h>
+#endif
 
 #include <mimic.h>
 
@@ -71,6 +74,7 @@ float synth_text(char *text)
     return dur;
 }
 
+#ifdef _OPENMP
 int main()
 {
     init();
@@ -84,3 +88,9 @@ int main()
     mimic_exit();
     return 0;
 }
+#else
+int main()
+{
+    return 0;
+}
+#endif
