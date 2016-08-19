f8cdf3f1b83072c9d4acc190d4ffbabe24c8acce
Åke Forslund <ake.forslund@gmail.com>
2016-08-19 13:15:03 +0200

    Create test executable

---
+++ main/mimic_test_voice_load.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "mimic.h"
+
+int main(int argc, char *argv[])
+{
+   int i, num;
+   long acc = 0;
+   struct timespec t1;
+   struct timespec t2;
+   cst_voice * v;
+    mimic_init();
+    mimic_set_lang_list();      /* defined at compilation time */
+    num = atoi(argv[2]);
+   for(i = 0; i < num; i++)
+   {
+      clock_gettime(CLOCK_REALTIME, &t1);
+      v = mimic_voice_load(argv[1]);
+      clock_gettime(CLOCK_REALTIME, &t2);
+      acc += (t2.tv_sec - t1.tv_sec) * 1000000000 + (t2.tv_nsec - t1.tv_nsec);
+   }
+      printf("voice name: %s, %ld\n", v->name, acc);
+   return 0;
+}
