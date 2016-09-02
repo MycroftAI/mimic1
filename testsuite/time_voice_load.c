559bcbebd0234144230a8eed17b7ca3c3ec21c91
Åke Forslund <ake.forslund@gmail.com>
2016-09-02 07:40:45 +0200

    Make time_voice_load more userfriendly.

--- testsuite/time_voice_load.c
+++ testsuite/time_voice_load.c
@@ -3,23 +3,43 @@
 #include <time.h>
 #include "mimic.h"
 
+void mimic_set_lang_list();
+
+void print_usage(char *prog_name)
+{
+    printf("%s voice-file [number of loads]\n", prog_name);
+}
+
+
 int main(int argc, char *argv[])
 {
-   int i, num;
-   long acc = 0;
-   struct timespec t1;
-   struct timespec t2;
-   cst_voice * v;
+    int i, num;
+    long acc = 0;
+    struct timespec t1;
+    struct timespec t2;
+    cst_voice * v = NULL;
     mimic_init();
-    mimic_set_lang_list();      /* defined at compilation time */
-    num = atoi(argv[2]);
-   for(i = 0; i < num; i++)
-   {
-      clock_gettime(CLOCK_REALTIME, &t1);
-      v = mimic_voice_load(argv[1]);
-      clock_gettime(CLOCK_REALTIME, &t2);
-      acc += (t2.tv_sec - t1.tv_sec) * 1000000000 + (t2.tv_nsec - t1.tv_nsec);
-   }
-      printf("voice name: %s, %ld\n", v->name, acc);
-   return 0;
+    mimic_set_lang_list();
+    if (argc < 2)
+    {
+        print_usage(argv[0]);
+        return -1;
+    }
+    else if (argc < 3)
+        num = 1;
+    else
+        num = atoi(argv[2]);
+
+    for(i = 0; i < num; i++)
+    {
+        clock_gettime(CLOCK_REALTIME, &t1);
+        v = mimic_voice_load(argv[1]);
+        clock_gettime(CLOCK_REALTIME, &t2);
+        acc += (t2.tv_sec - t1.tv_sec) * 1000000000 + (t2.tv_nsec - t1.tv_nsec);
+    }
+    if (v != NULL)
+    {
+        printf("voice name: %s, %ld\n", v->name, acc);
+    }
+    return 0;
 }
