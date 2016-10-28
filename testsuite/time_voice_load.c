605085fd820b188d636e902f21acc568acb27eaa
Åke Forslund <ake.forslund@gmail.com>
2016-10-28 13:49:41 +0200

    Add os X specific get time function in time_voice_load.c

--- testsuite/time_voice_load.c
+++ testsuite/time_voice_load.c
@@ -3,6 +3,26 @@
 #include <time.h>
 #include "mimic.h"
 
+#ifdef __MACH__
+#include <mach/clock.h>
+#include <mach/mach.h>
+#endif
+
+void current_utc_time(struct timespec *ts)
+{
+#ifdef __MACH__ // OS X does not have clock_gettime, use clock_get_time
+   clock_serv_t cclock;
+   mach_timespec_t mts;
+   host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
+   clock_get_time(cclock, &mts);
+   mach_port_deallocate(mach_task_self(), cclock);
+   ts->tv_sec = mts.tv_sec;
+   ts->tv_nsec = mts.tv_nsec;
+#else
+   clock_gettime(CLOCK_REALTIME, ts);
+#endif
+}
+
 void mimic_set_lang_list();
 
 
@@ -33,9 +53,9 @@ int main(int argc, char *argv[])
 
     for(i = 0; i < num; i++)
     {
-        clock_gettime(CLOCK_REALTIME, &t1);
+        current_utc_time(&t1);
         v = mimic_voice_load(argv[1]);
-        clock_gettime(CLOCK_REALTIME, &t2);
+        current_utc_time(&t2);
         acc += (t2.tv_sec - t1.tv_sec) * 1000000000 + (t2.tv_nsec - t1.tv_nsec);
     }
     if (v != NULL)
