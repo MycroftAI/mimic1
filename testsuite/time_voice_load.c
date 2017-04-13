cbecc82a79251a8c60632e4a7cbcdd95f0f2e4b7
Åke Forslund <ake.forslund@gmail.com>
2017-04-13 07:16:39 +0200

    Enable Posix features for time_voice_load.c.

--- testsuite/time_voice_load.c
+++ testsuite/time_voice_load.c
@@ -1,3 +1,8 @@
+#if __STDC_VERSION__ >= 199901L
+#define _XOPEN_SOURCE 600
+#else
+#define _XOPEN_SOURCE 500
+#endif /* __STDC_VERSION__ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
