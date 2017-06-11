79a60db9778f349fdc5a8e2b71e9ecfd6de57360
Sergio Oller <sergioller@gmail.com>
2017-06-11 10:52:43 +0200

    Refactor run_testsuite. Add ios universal build

--- run_testsuite.sh
+++ run_testsuite.sh
@@ -3,6 +3,7 @@
 if [ "$#" -eq 0 ]; then
   echo "./run_testsuite.sh requires a task/build target to be done"
   echo "  - ./run_testsuite.sh osx (OSX install dependencies, build and test)"
+  echo "  - ./run_testsuite.sh ios (OSX install dependencies, build universal lib)"
   echo "  - ./run_testsuite.sh coverage (Build with code coverage)"
   echo "  - ./run_testsuite.sh shared (Build with shared libraries)"
   echo "  - ./run_testsuite.sh gcc6 (Build using gcc6)"
@@ -13,7 +14,35 @@ fi
 
 WHAT_TO_RUN="$1"
 
-MIMIC_TOP_SRCDIR=`dirname \`readlink -f "$0"\``
+
+# emulates readlink -f, not available on osx
+readlink2()
+{
+MYWD=`pwd`
+TARGET_FILE=$1
+
+cd `dirname $TARGET_FILE`
+TARGET_FILE=`basename $TARGET_FILE`
+
+# Iterate down a (possible) chain of symlinks
+while [ -L "$TARGET_FILE" ]
+do
+    TARGET_FILE=`readlink $TARGET_FILE`
+    cd `dirname $TARGET_FILE`
+    TARGET_FILE=`basename $TARGET_FILE`
+done
+
+# Compute the canonicalized name by finding the physical path 
+# for the directory we're in and appending the target file.
+PHYS_DIR=`pwd -P`
+RESULT=$PHYS_DIR/$TARGET_FILE
+# restore working directory
+cd "$MYWD"
+echo $RESULT
+}
+
+
+export MIMIC_TOP_SRCDIR=`dirname \`readlink2 "$0"\``
 
 export MANIFEST_TOOL=:
 
@@ -134,93 +163,21 @@ fix_portaudio_pc_file()
     fi
 }
 
-case "${WHAT_TO_RUN}" in
-  osx)
-    brew install pkg-config automake libtool portaudio pcre2
+run_mimic_autogen()
+{
     (cd "${MIMIC_TOP_SRCDIR}" && ./autogen.sh) || exit 1
-    ./configure || exit 1
-    make -j ${NCORES} || exit 1
-    make check || exit 1
-    ;;
-  coverage)
-    MIMIC_INSTALL_DIR=`pwd`"/install/${WHAT_TO_RUN}"
-    cd "${MIMIC_TOP_SRCDIR}"
-    ./dependencies.sh --prefix="${MIMIC_INSTALL_DIR}" || exit 1
-    PKG_CONFIG_PATH=`pkg-config --variable pc_path pkg-config`
-    PKG_CONFIG_PATH="${MIMIC_INSTALL_DIR}/lib/pkgconfig:${PKG_CONFIG_PATH}"
-    ./autogen.sh || exit 1
-    ./configure  CFLAGS="$CFLAGS --coverage --no-inline" LDFLAGS="$LDFLAGS --coverage" PKG_CONFIG_PATH="${PKG_CONFIG_PATH}" || exit 1
-    make -j ${NCORES} || exit 1
-    make check || exit 1
-    ./do_gcov.sh
-    ;;
-  shared)
+}
+
+set_build_and_install_dir()
+{
     MIMIC_INSTALL_DIR=`pwd`"/install/${WHAT_TO_RUN}"
     WORKDIR=`pwd`"/builds/${WHAT_TO_RUN}"
     mkdir -p "${MIMIC_INSTALL_DIR}"
     mkdir -p "${WORKDIR}"
-    (cd "${WORKDIR}" && ${MIMIC_TOP_SRCDIR}/dependencies.sh --prefix="${MIMIC_INSTALL_DIR}") || exit 1
-    (cd "${MIMIC_TOP_SRCDIR}" && ./autogen.sh) || exit 1
-    PKG_CONFIG_PATH=`pkg-config --variable pc_path pkg-config`
-    PKG_CONFIG_PATH="${MIMIC_INSTALL_DIR}/lib/pkgconfig:${PKG_CONFIG_PATH}"
-    (cd "$WORKDIR" && \
-      ${MIMIC_TOP_SRCDIR}/configure --enable-shared \
-     --prefix="${MIMIC_INSTALL_DIR}" \
-          CFLAGS="$CFLAGS --std=c99" \
-          PKG_CONFIG_PATH="${PKG_CONFIG_PATH}" && \
-      make -j ${NCORES} && \
-      make check ) || exit 1
-    ;;
-  gcc6)
-    export CC="/usr/bin/gcc-6"
-    export CXX="/usr/bin/g++-6"
-    MIMIC_INSTALL_DIR=`pwd`"/install/${WHAT_TO_RUN}"
-    WORKDIR=`pwd`"/builds/${WHAT_TO_RUN}"
-    mkdir -p "${MIMIC_INSTALL_DIR}"
-    mkdir -p "${WORKDIR}"
-    (cd "${WORKDIR}" && ${MIMIC_TOP_SRCDIR}/dependencies.sh --prefix="${MIMIC_INSTALL_DIR}") || exit 1
-    (cd "${MIMIC_TOP_SRCDIR}" && ./autogen.sh) || exit 1
-    export CFLAGS="$CFLAGS --std=c99"
-    PKG_CONFIG_PATH=`pkg-config --variable pc_path pkg-config`
-    PKG_CONFIG_PATH="${MIMIC_INSTALL_DIR}/lib/pkgconfig:${PKG_CONFIG_PATH}"
-    (cd "$WORKDIR" && \
-      ${MIMIC_TOP_SRCDIR}/configure --enable-shared \
-     --prefix="${MIMIC_INSTALL_DIR}" \
-          CFLAGS="$CFLAGS --std=c99" \
-          PKG_CONFIG_PATH="${PKG_CONFIG_PATH}" && \
-      make -j ${NCORES} && \
-      make check ) || exit 1
-    ;;
-  arm-linux-gnueabihf-gcc)
-    MIMIC_INSTALL_DIR=`pwd`"/install/${WHAT_TO_RUN}"
-    WORKDIR=`pwd`"/builds/${WHAT_TO_RUN}"
-    mkdir -p "${MIMIC_INSTALL_DIR}"
-    mkdir -p "${WORKDIR}"
-    (cd "${MIMIC_TOP_SRCDIR}" && ./autogen.sh) || exit 1
-    export BUILD_TRIPLET=`sh ./config/config.guess`
-    export HOST_TRIPLET="arm-linux-gnueabihf"
-  crosscompile_dependencies
-    crosscompile_mimic --with-audio=none    
-    ;;
-  winbuild)
-    MIMIC_INSTALL_DIR=`pwd`"/install/${WHAT_TO_RUN}"
-    WORKDIR=`pwd`"/builds/${WHAT_TO_RUN}"
-    mkdir -p "${MIMIC_INSTALL_DIR}"
-    mkdir -p "${WORKDIR}"
-    (cd "${MIMIC_TOP_SRCDIR}" && ./autogen.sh) || exit 1
-    export BUILD_TRIPLET=`sh ./config/config.guess`
-    if [ `which i586-mingw32msvc-gcc` ]; then
-        export HOST_TRIPLET="i586-mingw32msvc"
-    elif [ `which i686-w64-mingw32-gcc` ]; then
-        export HOST_TRIPLET="i686-w64-mingw32"
-    else
-        echo "No windows cross-compiler found"
-        exit 1
-    fi
-    crosscompile_dependencies
-    crosscompile_portaudio --disable-shared --enable-static
-    crosscompile_mimic  --disable-shared --enable-static --with-audio=portaudio
-    put_dll_in_bindir
+}
+
+test_windows_build()
+{
     # Test mimic:
     cd "$WORKDIR" || exit 1
     if [ "x${DISPLAY}" = "x" ]; then
@@ -229,13 +186,10 @@ case "${WHAT_TO_RUN}" in
       wine "${MIMIC_INSTALL_DIR}/bin/mimic.exe" -voice ap -t "hello world" "hello_world_winbuild.wav" || exit 1
     fi
     echo "fbe80cc64ed244c0ee02c62a8489f182  hello_world_winbuild.wav" | md5sum -c || exit 1
-    ;;
-  winbuild_shared)
-    MIMIC_INSTALL_DIR=`pwd`"/install/${WHAT_TO_RUN}"
-    WORKDIR=`pwd`"/builds/${WHAT_TO_RUN}"
-    mkdir -p "${MIMIC_INSTALL_DIR}"
-    mkdir -p "${WORKDIR}"
-    (cd "${MIMIC_TOP_SRCDIR}" && ./autogen.sh) || exit 1
+}
+
+set_windows_triplet()
+{
     export BUILD_TRIPLET=`sh ./config/config.guess`
     if [ `which i586-mingw32msvc-gcc` ]; then
         export HOST_TRIPLET="i586-mingw32msvc"
@@ -245,19 +199,180 @@ case "${WHAT_TO_RUN}" in
         echo "No windows cross-compiler found"
         exit 1
     fi
+}
+
+compile_dependencies()
+{
+    (cd "${WORKDIR}" && ${MIMIC_TOP_SRCDIR}/dependencies.sh --prefix="${MIMIC_INSTALL_DIR}") || exit 1
+}
+
+compile_mimic()
+{
+  (cd "$WORKDIR" && \
+    ${MIMIC_TOP_SRCDIR}/configure \
+      PKG_CONFIG_PATH="${MIMIC_INSTALL_DIR}/lib/pkgconfig" \
+      --prefix="${MIMIC_INSTALL_DIR}" \
+      "$@" && \
+    make -j ${NCORES} && \
+    make check ) || exit 1
+}
+
+do_gcov()
+{
+  for direct in $(find . -type d); do
+    (cd "$direct"; 
+      for file in $(ls | grep "\\.gcno$"); do
+        echo "$PWD";
+        gcov -b "$file";
+      done)
+  done
+}
+
+case "${WHAT_TO_RUN}" in
+  osx)
+    brew install pkg-config automake libtool portaudio pcre2
+    set_build_and_install_dir
+    run_mimic_autogen
+    compile_mimic
+    ;;
+  ios)
+    brew install pkg-config automake libtool
+    run_mimic_autogen
+    # arm64
+    MIMIC_INSTALL_DIR=`pwd`"/install/ios_arm64"
+    WORKDIR=`pwd`"/builds/ios_arm64"
+    mkdir -p "${MIMIC_INSTALL_DIR}"
+    mkdir -p "${WORKDIR}"
+  (cd "$WORKDIR" && \
+    ${MIMIC_TOP_SRCDIR}/configure \
+      PKG_CONFIG_PATH="${MIMIC_INSTALL_DIR}/lib/pkgconfig" \
+      PKG_CONFIG_LIBDIR="" \
+      --prefix="${MIMIC_INSTALL_DIR}" \
+      --with-audio=none \
+      CFLAGS="-Ofast -mios-version-min=5.0" LDFLAGS="-flto" CC="xcrun -sdk iphoneos clang -arch arm64" --host=arm && \
+    make -j ${NCORES} && \
+    make install) || exit 1
+
+    # armv7
+    MIMIC_INSTALL_DIR=`pwd`"/install/ios_armv7"
+    WORKDIR=`pwd`"/builds/ios_armv7"
+    mkdir -p "${MIMIC_INSTALL_DIR}"
+    mkdir -p "${WORKDIR}"
+  (cd "$WORKDIR" && \
+    ${MIMIC_TOP_SRCDIR}/configure \
+      PKG_CONFIG_PATH="${MIMIC_INSTALL_DIR}/lib/pkgconfig" \
+      PKG_CONFIG_LIBDIR="" \
+      --prefix="${MIMIC_INSTALL_DIR}" \
+      --with-audio=none \
+      CFLAGS="-Ofast -mios-version-min=5.0" LDFLAGS="-flto" CC="xcrun -sdk iphoneos clang -arch armv7" --host=arm && \
+    make -j ${NCORES} && \
+    make install) || exit 1
+
+    # armv7s
+    MIMIC_INSTALL_DIR=`pwd`"/install/ios_armv7s"
+    WORKDIR=`pwd`"/builds/ios_armv7s"
+    mkdir -p "${MIMIC_INSTALL_DIR}"
+    mkdir -p "${WORKDIR}"
+  (cd "$WORKDIR" && \
+    ${MIMIC_TOP_SRCDIR}/configure \
+      PKG_CONFIG_PATH="${MIMIC_INSTALL_DIR}/lib/pkgconfig" \
+      PKG_CONFIG_LIBDIR="" \
+      --prefix="${MIMIC_INSTALL_DIR}" \
+      --with-audio=none \
+      CFLAGS="-Ofast -mios-version-min=5.0" LDFLAGS="-flto" CC="xcrun -sdk iphoneos clang -arch armv7s" --host=arm && \
+    make -j ${NCORES} && \
+    make install) || exit 1
+
+  # i386
+    MIMIC_INSTALL_DIR=`pwd`"/install/ios_i386"
+    WORKDIR=`pwd`"/builds/ios_i386"
+    mkdir -p "${MIMIC_INSTALL_DIR}"
+    mkdir -p "${WORKDIR}"
+  (cd "$WORKDIR" && \
+    ${MIMIC_TOP_SRCDIR}/configure \
+      PKG_CONFIG_PATH="${MIMIC_INSTALL_DIR}/lib/pkgconfig" \
+      PKG_CONFIG_LIBDIR="" \
+      --prefix="${MIMIC_INSTALL_DIR}" \
+      --with-audio=none \
+      CFLAGS="-Ofast -mios-version-min=5.0" LDFLAGS="-flto" CC="xcrun -sdk iphonesimulator clang -arch i386" && \
+    make -j ${NCORES} && \
+    make install) || exit 1
+
+  # x86_64
+    MIMIC_INSTALL_DIR=`pwd`"/install/ios_x86_64"
+    WORKDIR=`pwd`"/builds/ios_x86_64"
+    mkdir -p "${MIMIC_INSTALL_DIR}"
+    mkdir -p "${WORKDIR}"
+  (cd "$WORKDIR" && \
+    ${MIMIC_TOP_SRCDIR}/configure \
+      PKG_CONFIG_PATH="${MIMIC_INSTALL_DIR}/lib/pkgconfig" \
+      PKG_CONFIG_LIBDIR="" \
+      --prefix="${MIMIC_INSTALL_DIR}" \
+      --with-audio=none \
+      CFLAGS="-Ofast -mios-version-min=5.0" LDFLAGS="-flto" CC="xcrun -sdk iphonesimulator clang -arch x86_64" && \
+    make -j ${NCORES} && \
+    make install) || exit 1
+
+    echo "Building Universal Static Lib"
+    mkdir -p "install/ios_universal"
+    # run lipo to link binaries
+    xcrun lipo -create $(find install/*/lib -name "*.a") -o "install/ios_universal/mimic_bundle.a"
+    cp -r "install/ios_armv7/include" "install/ios_universal/"
+    echo "Universal lib found in install/ios_universal"
+    echo "If all voices have been linked then the resulting universal lib may be VERY large"
+
+    ;;
+  coverage)
+    MIMIC_INSTALL_DIR=`pwd`"/install/${WHAT_TO_RUN}"
+    WORKDIR=`pwd` # do_gcov needs to be adapted to out of tree builds
+    mkdir -p "${MIMIC_INSTALL_DIR}"
+    compile_dependencies
+    run_mimic_autogen
+    compile_mimic CFLAGS="$CFLAGS --coverage --no-inline" LDFLAGS="$LDFLAGS --coverage" 
+    do_gcov
+    ;;
+  shared)
+    set_build_and_install_dir
+    compile_dependencies
+    run_mimic_autogen
+    compile_mimic --enable-shared CFLAGS="$CFLAGS --std=c99"
+    ;;
+  gcc6)
+      export CC="/usr/bin/gcc-6"
+      export CXX="/usr/bin/g++-6"
+    set_build_and_install_dir
+    compile_dependencies
+    run_mimic_autogen
+    compile_mimic --enable-shared CFLAGS="$CFLAGS --std=c99"
+    ;;
+  arm-linux-gnueabihf-gcc)
+    set_build_and_install_dir
+    run_mimic_autogen
+    export BUILD_TRIPLET=`sh ./config/config.guess`
+    export HOST_TRIPLET="arm-linux-gnueabihf"
+    crosscompile_dependencies
+    crosscompile_mimic --with-audio=none    
+    ;;
+  winbuild)
+    set_build_and_install_dir
+    run_mimic_autogen
+    set_windows_triplet
+    crosscompile_dependencies
+    crosscompile_portaudio --disable-shared --enable-static
+    crosscompile_mimic  --disable-shared --enable-static --with-audio=portaudio
+    put_dll_in_bindir
+    test_windows_build
+    ;;
+  winbuild_shared)
+    set_build_and_install_dir
+    run_mimic_autogen
+    set_windows_triplet
     crosscompile_dependencies
     crosscompile_portaudio --enable-shared
     fix_portaudio_pc_file
     crosscompile_mimic --enable-shared --with-audio=portaudio
     put_dll_in_bindir
-    # Test mimic:
-    cd "$WORKDIR" || exit 1
-    if [ "x${DISPLAY}" = "x" ]; then
-      xvfb-run wine "${MIMIC_INSTALL_DIR}/bin/mimic.exe" -voice ap -t "hello world" "hello_world_winbuild.wav" || exit 1
-    else
-      wine "${MIMIC_INSTALL_DIR}/bin/mimic.exe" -voice ap -t "hello world" "hello_world_winbuild.wav" || exit 1
-    fi
-    echo "fbe80cc64ed244c0ee02c62a8489f182  hello_world_winbuild.wav" | md5sum -c || exit 1
+    test_windows_build
     ;;
   *)
     echo "Unknown WHAT_TO_RUN: ${WHAT_TO_RUN}"
